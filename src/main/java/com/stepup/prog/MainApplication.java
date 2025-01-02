14f8ee412d8e26c4e02147237bbfca6dcf89fbae
Владимир Чекис <gershvin@vk.com>
2025-01-02 17:18:43 +0500

    Изменена логика и добавлена коробка

--- src/main/java/com/stepup/prog/MainApplication.java
+++ src/main/java/com/stepup/prog/MainApplication.java
@@ -3,6 +3,8 @@ package com.stepup.prog;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("1111111");
+        System.out.println("11111001");
+        System.out.println("222222");
+        System.out.println("3333333");
     }
 }
