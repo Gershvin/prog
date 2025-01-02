44a96e8cac23eca8234d1bacdc4d260516e7b262
Владимир Чекис <gershvin@vk.com>
2025-01-02 17:21:16 +0500

    Изменён метод doSomething

--- src/main/java/com/stepup/prog/MainApplication.java
+++ src/main/java/com/stepup/prog/MainApplication.java
@@ -4,7 +4,11 @@ public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("11111001");
-        System.out.println("222222");
-        System.out.println("3333333");
+    }
+
+    public static void doSomething() {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
     }
 }
